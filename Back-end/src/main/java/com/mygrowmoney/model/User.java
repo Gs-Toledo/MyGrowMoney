fdcf9d9585402a99072c8773aa53b1e5b7fdd4a4
João Pedro Diniz <98347518+joaopedrodiniz@users.noreply.github.com>
2024-10-04 01:09:54 +0000

    feat: Criando Getters e setters

--- Back-end/src/main/java/com/mygrowmoney/model/User.java
+++ Back-end/src/main/java/com/mygrowmoney/model/User.java
@@ -3,7 +3,7 @@ package com.exemplo.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User {
 
     @Id
@@ -16,4 +16,27 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
