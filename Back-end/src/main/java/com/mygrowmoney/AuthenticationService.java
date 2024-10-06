bc70117f3656cccec94a2716373067b08d8bd48c
Isaac Ferreira <zaclummys@gmail.com>
2024-10-06 00:34:23 +0000

    Implement outputs

--- Back-end/src/main/java/com/mygrowmoney/AuthenticationService.java
+++ Back-end/src/main/java/com/mygrowmoney/AuthenticationService.java
@@ -1,41 +1,49 @@
 package com.mygrowmoney;
 
+import com.mygrowmoney.infra.jwt.JwtService;
 import com.mygrowmoney.models.User;
 import com.mygrowmoney.repositories.UserRepository;
-import com.mygrowmoney.services.AuthService.AuthServiceException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
     
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
     
-    private final AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    public AuthenticationService(
-        UserRepository userRepository,
-        AuthenticationManager authenticationManager,
-        PasswordEncoder passwordEncoder
-    ) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    public User signUp (
+    public SignUpOutput signUp (
         String name,
         String email,
         String password
     ) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
+
         User userFoundByEmail = userRepository.findByEmail(email);
 
         if (userFoundByEmail != null) {
-            throw new RuntimeException("Email already registered: " + email);
+            return new SignUpOutput()
+                .withSuccess(false)
+                .withMessage("Email already registered");
         }
 
         User user = new User(
@@ -44,20 +52,84 @@ public class AuthenticationService {
             password
         );
 
-        return userRepository.save(user);
+        userRepository.save(user);
+
+        return new SignUpOutput()
+            .withSuccess(true)
+            .withMessage("Signed up successfully");
     }
 
-    public User signIn (String email, String password) {
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(email, password)
-        );
+    public SignInOutput signIn (
+        String email,
+        String password
+    ) {
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
+
+        try {
+            authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+            );
+        } catch (BadCredentialsException exception) {
+            return new SignInOutput()
+                .withSuccess(false)
+                .withMessage("User not found or password does not match");
+        }
 
         User user = userRepository.findByEmail(email);
 
-        if (user == null) {
-            throw new RuntimeException("User not found: " + user);
+        String accessToken = jwtService.encode(user.getId().toString());
+
+        return new SignInOutput()
+            .withSuccess(true)
+            .withMessage("Signed in sucessfully")
+            .withAccessToken(accessToken);
+    }
+
+    public static class SignInOutput {
+        public Boolean success;
+        public String message;
+        public String accessToken;
+
+        public SignInOutput withSuccess (Boolean success) {
+            this.success = success;
+
+            return this;
         }
 
-        return user;
+        public SignInOutput withMessage (String message) {
+            this.message = message;
+
+            return this;
+        }
+
+        public SignInOutput withAccessToken (String accessToken) {
+            this.accessToken = accessToken;
+
+            return this;
+        }
     }
+
+    public static class SignUpOutput {
+        public Boolean success;
+        public String message;
+
+        public SignUpOutput withSuccess (Boolean success) {
+            this.success = success;
+
+            return this;
+        }
+
+        public SignUpOutput withMessage (String message) {
+            this.message = message;
+
+            return this;
+        }
+    }
+
 }
