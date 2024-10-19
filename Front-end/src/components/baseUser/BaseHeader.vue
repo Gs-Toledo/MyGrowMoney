c6134fa05b90cac5419cd43ddf6764dea659b39b
Giovanni Toledo <107721397+Gs-Toledo@users.noreply.github.com>
2024-10-19 00:41:51 +0000

    Feat: sidebar para usuario logado

--- Front-end/src/components/baseUser/BaseHeader.vue
+++ Front-end/src/components/baseUser/BaseHeader.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app-bar :elevation="2">
-    <template v-slot:prepend>
-      <v-app-bar-nav-icon>
-        <v-icon icon="mdi-menu"></v-icon>
-      </v-app-bar-nav-icon>
-    </template>
-    <v-app-bar-title><router-link to="/home">My Grow Money</router-link></v-app-bar-title>
+    <v-app-bar-title
+      ><router-link class="text-black hover:text-blue-700" to="/home"
+        >My Grow Money</router-link
+      ></v-app-bar-title
+    >
     <template v-slot:append>
       <v-btn icon="mdi-dots-vertical"></v-btn>
       <v-btn icon="mdi-logout" @click="showLogoutModal = true"></v-btn>
--- Front-end/src/components/baseUser/BaseUserTemplate.vue
+++ Front-end/src/components/baseUser/BaseUserTemplate.vue
@@ -2,6 +2,24 @@
   <v-app>
     <base-header />
     <v-main>
+      <v-navigation-drawer expand-on-hover rail>
+        <v-list>
+          <v-list-item
+            :prepend-avatar="userImg"
+            subtitle="Usuario X"
+            title="User Image"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="Sessão 1"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Sessão 2"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Sessão 3"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
       <v-container>
         <slot></slot>
       </v-container>
@@ -10,10 +28,15 @@
 </template>
 
 <script>
-import BaseHeader from "@/components/baseUser/BaseHeader.vue";
+import BaseHeader from '@/components/baseUser/BaseHeader.vue'
 export default {
   components: {
-    BaseHeader,
+    BaseHeader
   },
-};
+  data() {
+    return {
+      userImg: 'https://w1.pngwing.com/pngs/386/684/png-transparent-face-icon-user-icon-design-user-profile-share-icon-avatar-black-and-white-silhouette.png'
+    }
+  }
+}
 </script>
