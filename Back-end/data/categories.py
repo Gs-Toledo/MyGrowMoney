36ae465531cda99a75b59aadd0727f0d6ea45cbd
João Pedro Diniz <98347518+joaopedrodiniz@users.noreply.github.com>
2024-11-21 00:56:06 +0000

    fix(categories.py): corrigindo o problema de privacidade de dados

--- Back-end/data/categories.py
+++ Back-end/data/categories.py
@@ -1,12 +1,14 @@
 from uuid import uuid4
-from peewee import Model, UUIDField, CharField, FloatField
+from peewee import Model, UUIDField, CharField, FloatField, ForeignKeyField
 from data.database import database
 
+from data.users import User
 
 class Category(Model):
     class Meta:
         database = database
 
+    user = ForeignKeyField(User, backref='categories')
     id = UUIDField(primary_key=True)
     name = CharField()  # Nome da categoria
     limit = FloatField(default=0) # Limite de gastos 
